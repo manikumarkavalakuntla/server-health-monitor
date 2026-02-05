ec3c400def29c2027934b20ff14f2483d78634a3
root <root@ip-172-31-48-46.ec2.internal>
2026-02-05 09:44:33 +0000

    Initial Project Setup is completed

---
+++ scripts/health_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#################################################################################################################################################################################################
+# Author	: Mani Kumar
+# Date		: 05/02/2026
+# Project Name	: Server Health Monitor
+# Purpose	: To collects the system health metrics, saves it to files, and tracks everything using git & github
+# Usage 	: sh /home/manikumar/server-health-monitor/scripts/health_check.sh
+# Constraints 	:
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 1.print date and time
+# 2.show:
+# 	CPU Usage
+# 	Memory Usage
+# 	Disk Usage
+# 3.Append output to /logs/health.log
+# 4.use:
+# 	Variables
+# 	Functions
+# 	At least one if condition
+#
+# ##############################################################################################################################################################################################
+
+
