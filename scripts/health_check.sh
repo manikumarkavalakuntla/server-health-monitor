450250b342dd1fe6844f5ad1af0061a83e3b6588
root <root@ip-172-31-48-46.ec2.internal>
2026-02-11 13:00:19 +0000

    The script optimised to fix the architectural issues and added logs ,exit codes and removed duplicates

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -23,6 +23,14 @@
 
 DATE=$(date +"%Y-%m-%d %H:%M:%S")
 HOSTNAME=$(hostname)
+LOG_FILE="/home/manikumar/server-health-monitor/logs/health.log"
+
+# -------------------- Threshold Configuration --------------------
+
+CRITICAL_THRESHOLD=90
+WARNING_THRESHOLD=75
+GLOBAL_EXIT_CODE=0
+
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 
@@ -32,67 +40,76 @@ echo "Hostname    : $HOSTNAME"
 
 echo "------------------------------------------------------------------------------------------------------------------------------"
 
+get_metric_status(){
+	
+	local METRIC_VALUE=$1
+	local STATUS=""
+        local CODE=0
+
+	if awk "BEGIN {exit !($METRIC_VALUE >= $CRITICAL_THRESHOLD)}";then
+		
+		echo "[	CRITICAL ]"
+		exit 2
+
+	elif awk "BEGIN {exit !($METRIC_VALUE >= $WARNING_THRESHOLD)}";then
+
+		echo "[ WARNING ]"
+		exit 1
+	
+	else
+		echo "[ OK ]"
+		exit 0
+	
+	fi
+
+#----------------------------- Track highest severity ----------------------------------------
+
+    if [[ $CODE -gt $GLOBAL_EXIT_CODE ]]; then
+
+        GLOBAL_EXIT_CODE=$CODE
+    fi
+
+    echo "$STATUS"
+}
+
+log_message() {
+    local MESSAGE="$1"
+    echo "$MESSAGE" >> "$LOG_FILE"
+}
+
+
 print_server_health_metrics(){
 
 	GET_CPU=$(check_cpu_usage)
-	GET_MEM=$(check_memory_usage)
-	GET_DISK=$(check_disk_usage)
+        GET_MEM=$(check_memory_usage)
+        GET_DISK=$(check_disk_usage)
 
-	CPU_STATUS=""
-	MEM_STATUS=""
-	DISK_STATUS=""
+ 	CPU_STATUS=$(get_metric_status "$GET_CPU")
+        MEM_STATUS=$(get_metric_status "$GET_MEM")
+        DISK_STATUS=$(get_metric_status "$GET_DISK")
 
-	if awk "BEGIN {exit !($GET_CPU >= 90)}"; then
+#       printf "%-15s : %s%% %s\n" "CPU Usage" "$GET_CPU" "$CPU_STATUS"
+#       printf "%-15s : %s%% %s\n" "Memory Usage" "$GET_MEM" "$MEM_STATUS"
+#       printf "%-15s : %s%% %s\n" "DISK Usage" "$GET_DISK" "$DISK_STATUS"
 
-		CPU_STATUS=$(printf "%-15s : %s%% %s" "CPU Usage" "$GET_CPU" "[ CRITICAL ]")
+	CPU_LINE=$(printf "%-15s : %s%% %s" "CPU Usage" "$GET_CPU" "$CPU_STATUS")
+        MEM_LINE=$(printf "%-15s : %s%% %s" "Memory Usage" "$GET_MEM" "$MEM_STATUS")
+        DISK_LINE=$(printf "%-15s : %s%% %s" "DISK Usage" "$GET_DISK" "$DISK_STATUS")
 
-	elif awk "BEGIN {exit !($GET_CPU >= 75)}";then
+	echo "$CPU_LINE"
+        echo "$MEM_LINE"
+        echo "$DISK_LINE"
 
-		CPU_STATUS=$(printf "%-15s : %s%% %s" "CPU Usage" "$GET_CPU" "[ WARNING ]")
-
-	else
-		CPU_STATUS=$(printf "%-15s : %s%% %s" "CPU Usage" "$GET_CPU" "[ OK ]")	
-
-	fi
-
-	printf "%s\n" "$CPU_STATUS"
-
-	if awk "BEGIN {exit !($GET_MEM >= 90)}"; then
-
-		MEM_STATUS=$(printf "%-15s : %s%% %s" "Memory Usage" "$GET_MEM" "[ CRITICAL ]")
-
-	elif awk "BEGIN {exit !($GET_MEM >= 75)}"; then
-
-		MEM_STATUS=$(printf "%-15s : %s%% %s" "Memory Usage" "$GET_MEM" "[ WARNING ]")
-	
-	else
-		MEM_STATUS=$(printf "%-15s : %s%% %s" "Memory Usage" "$GET_MEM" "[ OK ]")
-
-	fi
-
-	printf "%s\n" "$MEM_STATUS"
-
-	if awk "BEGIN {exit !($GET_DISK >= 90)}"; then
-
-                DISK_STATUS=$(printf "%-15s : %s%% %s" "DISK Usage" "$GET_DISK" "[ CRITICAL ]")
-
-        elif awk "BEGIN {exit !($GET_DISK >= 75)}";then
-
-                DISK_STATUS=$(printf "%-15s : %s%% %s" "DISK Usage" "$GET_DISK" "[ WARNING ]")
-
-        else
-                DISK_STATUS=$(printf "%-15s : %s%% %s" "DISK Usage" "$GET_DISK" "[ OK ]")
-
-        fi
-
-	printf "%s\n" "$DISK_STATUS"
-
+	# Log them
+        log_message "$DATE - $CPU_LINE"
+        log_message "$DATE - $MEM_LINE"
+        log_message "$DATE - $DISK_LINE"
 
 }
 
+
 check_cpu_usage(){
 
-	#top -bn1 | grep "%Cpu(s)" | awk '{print 100-$8}'
 	mpstat 1 1 | awk '/Average/ {printf "%.2f\n", 100 - $NF}'
 }
 
@@ -107,4 +124,4 @@ check_disk_usage(){
 }
 
 print_server_health_metrics
-
+exit $GLOBAL_EXIT_CODE
